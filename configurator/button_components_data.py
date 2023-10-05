01ff07f12524aeb212a6dd0b03b6e0f29dea6488
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2023-10-05 22:32:29 +1100

    Added components

--- configurator/button_components_data.py
+++ configurator/button_components_data.py
@@ -19,6 +19,7 @@ PRESSEL_TYPES = (
 )
 
 CONTACT_ARRANGEMENT = (
+    ("", "Select contact arrangement"),
     ("1", "2 x N/O"),
     ("2", "2 x N/C"),
     ("3", "1 x N/O 1 x N/C"),
@@ -27,4 +28,76 @@ CONTACT_ARRANGEMENT = (
     ("6", "FLAT CONTACT 2 x N/O"),
     ("7", "Vertical / Horizontal (Micro MTA100)"),
     ("8", "3 Way Vertical (Micro)")
+)
+
+LED_VOLTAGE = (
+    ("", "Select LED voltage"),
+    ("1", "Non Illuminated"),
+    ("2", "12VDC (Phone & Alarm)"),
+    ("3", "24VDC"),
+    ("4", "110VDC"),
+    ("5", "Multivolt 12-110V AC/DC"),
+    ("6", "6VDC")
+)
+
+LED_COLOR = (
+    ("", "Select LED color"),
+    ("00", "Non Illuminated"),
+    ("01", "Opal"),
+    ("02", "Red"),
+    ("03", "Amber"),
+    ("04", "Blue"),
+    ("05", "Green"),
+    ("11", "Dual Opal"),
+    ("12", "Dual Red"),
+    ("13", "Dual Amber"),
+    ("14", "Dual Blue"),
+    ("15", "Dual Green"),
+    ("22", "Opal/Red"),
+    ("23", "Opal/Amber"),
+    ("24", "Opal/Blue"),
+    ("25", "Opal/Green"),
+    ("26", "Opal/Blue Type B LED (old type with leads)"),
+    ("27", "Amber/Green")
+)
+
+SURROUND_TYPE = (
+    ("", "Select surround type"),
+    ("1", "Flush"),
+    ("2", "Handicap (Standard)"),
+    ("3", "Extended")
+)
+
+SURROUND_COLOR = (
+    ("", "Select surround color"),
+    ("1", "Chrome (Standard)"),
+    ("2", "Black Anodized"),
+    ("3", "Brass"),
+    ("4", "Unfinished"),
+    ("5", "Anti-Bacterial Black Plastic"),
+    ("6", "Anti-Bacterial Green Plastic"),
+    ("7", "Chrome Exit")
+)
+
+PRESSEL_FINISH = (
+    ("", "Select pressel finish"),
+    ("1", "Stainless Steel Linished (Standard)"),
+    ("2", "Mirror Polished"),
+    ("3", "Brass Linished"),
+    ("4", "Brass Polished"),
+    ("5", "Red / Blue / Yellow / White / Green Anodised"),
+    ("6", "Black Anodized (EN)"),
+    ("7", "Green Exit (EN)"),
+    ("0", "Bronze / Special Pressel")
+)
+
+PRESSEL_MOULD_COLOR = (
+    ("", "Select pressel mould color"),
+    ("1", "Opal (Standard)"),
+    ("2", "Red"),
+    ("3", "Amber (Phone & Alarm)"),
+    ("4", "Blue"),
+    ("5", "Green"),
+    ("6", "Grey"),
+    ("7", "Black")
 )
