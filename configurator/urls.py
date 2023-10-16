6146f7e40485821c3a736579f98ba8c7b9a010a6
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2023-10-16 22:34:42 +1100

    Added path for AJAX request

--- configurator/urls.py
+++ configurator/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='config-home'),
     path('standard/', views.standard_button, name='stand-butt'),
+    path('get_contact_type', views.get_contact_type, name='contact-type')
 ]
