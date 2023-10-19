436354d0032b5d9fccaaa858101609a1e5bba9a7
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2023-10-19 22:28:38 +1100

    Amended func get_contact_type()

--- configurator/views.py
+++ configurator/views.py
@@ -7,7 +7,7 @@ from .models import ButtonBody, PresselType
 def index(request):
     context = {
         'title': 'Home page',
-        'buttons': 'buttons'
+        # 'buttons': 'buttons'
     }
     return render(request, 'configurator/home.html', context)
 
@@ -18,11 +18,9 @@ def standard_button(request):
         form = StandardButtonForm(request.POST)
 
         button_body = request.GET.get('button_body')
-        print(button_body)
 
         if form.is_valid():
             selected_body = form.cleaned_data['button_body']
-            print(selected_body)
             selected_pressel = form.cleaned_data['pressel_type']
             selected_contacts = form.cleaned_data['contact_arr']
 
@@ -49,9 +47,12 @@ def get_contact_type(request):
     button_body = request.GET.get('button_body')
     print(button_body)
     # Perform logic to retrieve pressel types based on the selected button_body
-    if button_body == '3' or button_body == 'Compact 3' or button_body == 'Compact 3P':
-        contact_types = ["2 x N/O", "2 x N/C", "1 x N/O 1 x N/C"]
-        return JsonResponse({'contact_types': contact_types})
+    if button_body == '3' or button_body == '8' or button_body == '7':
+        contact_types = {'type 1': "2 x N/O", 'type 2': "2 x N/C", 'type 3': "1 x N/O 1 x N/C"}
+        return JsonResponse(contact_types)
+    elif button_body == '4':
+        contact_types = {'type 4': "1 x N/O (Micro AMP)"}
+        return JsonResponse(contact_types)
     else:
         # If no contact types are available, return a suitable response, e.g., a 400 Bad Request.
         return HttpResponseBadRequest("Invalid button body selection")
