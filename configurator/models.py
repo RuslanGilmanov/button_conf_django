cd4dd8f4af64361443de0224f24bfcab574c5b32
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2023-10-24 22:59:33 +1100

    Added more models to build a 3-part kit

--- configurator/models.py
+++ configurator/models.py
@@ -2,16 +2,57 @@ from django.db import models
 
 
 class ButtonBody(models.Model):
-    code = models.CharField(max_length=5)
+    body_code = models.CharField(max_length=5)
     body = models.CharField(max_length=50)
 
     def __str__(self):
         return self.body
 
 
-class PresselType(models.Model):
-    code = models.CharField(max_length=5)
-    pressel = models.CharField(max_length=50)
+class ContactType(models.Model):
+    contact_code = models.CharField(max_length=5)
+    contact = models.CharField(max_length=50)
+    but_body = models.ForeignKey(ButtonBody, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.pressel
+        return self.contact
+
+
+class IlluminationColor(models.Model):
+    led_code = models.CharField(max_length=5)
+    led_color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.led_color
+
+
+class IlluminationVoltage(models.Model):
+    volt_code = models.CharField(max_length=5)
+    voltage = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.voltage
+
+
+class SurroundType(models.Model):
+    surround_code = models.CharField(max_length=5)
+    surround = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.surround
+
+
+class SurroundColor(models.Model):
+    sur_color_code = models.CharField(max_length=5)
+    surround_color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.surround_color
+
+
+class SurroundForm(models.Model):
+    shape_code = models.CharField(max_length=5)
+    surround_form = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.surround_form
