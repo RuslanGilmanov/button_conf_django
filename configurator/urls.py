11f7b8b1230e15a4724308513d97a464a8b296f2
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2023-10-26 23:28:07 +1100

    Deleted div tag in button_body fieled

--- configurator/urls.py
+++ configurator/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='config-home'),
     path('standard/', views.standard_button, name='stand-butt'),
-    path('get_contact_type', views.get_contact_type, name='contact-type')
+    path('get_contact_type', views.get_contact_type, name='contact-type'),
+    path('load_contact_types/', views.load_contact_types, name='load_contact_types')
 ]
