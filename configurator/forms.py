b6b44a06654daf29d76df552f33b83ee397e386c
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2023-10-26 23:22:17 +1100

    Replaced ChoiceField(s) with ModelChoiceField(s)

--- configurator/forms.py
+++ configurator/forms.py
@@ -1,27 +1,24 @@
 from django import forms
-from .models import ButtonBody, PresselType
-from .button_components_data import (
-    BUTTON_BODIES,
-    PRESSEL_TYPES,
-    CONTACT_ARRANGEMENT,
-    LED_VOLTAGE,
-    LED_COLOR,
-    SURROUND_TYPE,
-    SURROUND_COLOR,
-    PRESSEL_FINISH,
-    PRESSEL_MOULD_COLOR,
-
+from .models import (
+    ButtonBody,
+    ContactType,
+    ButtonBodyContactType,
+    IlluminationColor,
+    IlluminationVoltage,
+    SurroundType,
+    SurroundColor,
+    SurroundForm
 )
 
 
 class StandardButtonForm(forms.Form):
-    button_body = forms.ChoiceField(choices=BUTTON_BODIES, label='Button body')
-    contact_arr = forms.ChoiceField(label='Contacts')
-    pressel_type = forms.ChoiceField(choices=PRESSEL_TYPES, label='Pressel type')
-    led_voltage = forms.ChoiceField(choices=LED_VOLTAGE, label='LED voltage')
-    led_color = forms.ChoiceField(choices=LED_COLOR, label='LED color')
-    surround_type = forms.ChoiceField(choices=SURROUND_TYPE, label='Surround type')
-    surround_color = forms.ChoiceField(choices=SURROUND_COLOR, label='Surround color')
-    pressel_finish = forms.ChoiceField(choices=PRESSEL_FINISH, label='Pressel finish')
-    mould_color = forms.ChoiceField(choices=PRESSEL_MOULD_COLOR, label='Mould color')
+    button_body = forms.ModelChoiceField(queryset=ButtonBody.objects.all())
+    contact_type = forms.ModelChoiceField(queryset=ContactType.objects.none())
+    # pressel_type = forms.ModelChoiceField(choices=PRESSEL_TYPES, label='Pressel type')
+    # led_voltage = forms.ModelChoiceField(choices=LED_VOLTAGE, label='LED voltage')
+    # led_color = forms.ModelChoiceField(choices=LED_COLOR, label='LED color')
+    # surround_type = forms.ModelChoiceField(choices=SURROUND_TYPE, label='Surround type')
+    # surround_color = forms.ModelChoiceField(choices=SURROUND_COLOR, label='Surround color')
+    # pressel_finish = forms.ModelChoiceField(choices=PRESSEL_FINISH, label='Pressel finish')
+    # mould_color = forms.ModelChoiceField(choices=PRESSEL_MOULD_COLOR, label='Mould color')
 
