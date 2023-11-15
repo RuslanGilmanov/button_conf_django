dfcdfa026c467b2d83ee1bcbe5da09d8f497b087
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2023-11-15 20:47:24 +1100

    Added more fields into StandardButtonForm class

--- configurator/forms.py
+++ configurator/forms.py
@@ -15,11 +15,12 @@ class StandardButtonForm(forms.Form):
         queryset=ButtonBody.objects.all(),
         widget=forms.Select(attrs={"hx-get": "load_contact_types/", "hx-target": "#id_contact_type"}))
     contact_type = forms.ModelChoiceField(queryset=ContactType.objects.none())
+    led_voltage = forms.ModelChoiceField(queryset=IlluminationVoltage.objects.all())
+    led_color = forms.ModelChoiceField(queryset=IlluminationColor.objects.all())
+    surround_type = forms.ModelChoiceField(queryset=SurroundType.objects.all())
+    surround_color = forms.ModelChoiceField(queryset=SurroundColor.objects.all())
+    surround_form = forms.ModelChoiceField(queryset=SurroundForm.objects.all())
     # pressel_type = forms.ModelChoiceField(choices=PRESSEL_TYPES, label='Pressel type')
-    # led_voltage = forms.ModelChoiceField(choices=LED_VOLTAGE, label='LED voltage')
-    # led_color = forms.ModelChoiceField(choices=LED_COLOR, label='LED color')
-    # surround_type = forms.ModelChoiceField(choices=SURROUND_TYPE, label='Surround type')
-    # surround_color = forms.ModelChoiceField(choices=SURROUND_COLOR, label='Surround color')
     # pressel_finish = forms.ModelChoiceField(choices=PRESSEL_FINISH, label='Pressel finish')
     # mould_color = forms.ModelChoiceField(choices=PRESSEL_MOULD_COLOR, label='Mould color')
 
