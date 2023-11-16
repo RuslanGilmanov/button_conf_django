31f6077b671e8059c816ce0cdadae14d12e83bac
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2023-11-16 22:28:04 +1100

    Added load_colors path

--- configurator/urls.py
+++ configurator/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='config-home'),
     path('standard/', views.standard_button, name='stand-butt'),
     path('get_contact_type', views.get_contact_type, name='contact-type'),
-    path('standard/load_contact_types/', views.load_contact_types, name='load_contact_types')
+    path('standard/load_contact_types/', views.load_contact_types, name='load_contact_types'),
+    path('standard/load_colors/', views.load_colors, name='load_colors')
 ]
