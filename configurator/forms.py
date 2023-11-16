bcf652763dffe9b373fa820996b97aee223b4b08
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2023-11-16 22:26:54 +1100

    Changed led_voltage and led_color fileds in order to use htmx

--- configurator/forms.py
+++ configurator/forms.py
@@ -15,8 +15,10 @@ class StandardButtonForm(forms.Form):
         queryset=ButtonBody.objects.all(),
         widget=forms.Select(attrs={"hx-get": "load_contact_types/", "hx-target": "#id_contact_type"}))
     contact_type = forms.ModelChoiceField(queryset=ContactType.objects.none())
-    led_voltage = forms.ModelChoiceField(queryset=IlluminationVoltage.objects.all())
-    led_color = forms.ModelChoiceField(queryset=IlluminationColor.objects.all())
+    led_voltage = forms.ModelChoiceField(
+        queryset=IlluminationVoltage.objects.all(),
+        widget=forms.Select(attrs={"hx-get": "load_colors/", "hx-target": "#id_led_color"}))
+    led_color = forms.ModelChoiceField(queryset=IlluminationColor.objects.none())
     surround_type = forms.ModelChoiceField(queryset=SurroundType.objects.all())
     surround_color = forms.ModelChoiceField(queryset=SurroundColor.objects.all())
     surround_form = forms.ModelChoiceField(queryset=SurroundForm.objects.all())
@@ -27,8 +29,10 @@ class StandardButtonForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        if "button_body" in self.data:
+        if "button_body" in self.data and "led_color" in self.data:
             body_id = int(self.data.get("button_body"))
             button_body = ButtonBody.objects.get(id=body_id)
+            led_id = int(self.data.get("led_voltage"))
+            led_voltage = IlluminationVoltage.objects.get(id=led_id)
             self.fields["contact_type"].queryset = button_body.contact_types.all()
-
+            self.fields["led_color"].queryset = led_voltage.led_colors.all()
