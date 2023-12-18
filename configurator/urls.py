2e623f113f2c14a0dbdb98353210bebbbabefb46
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2023-12-18 22:39:10 +1100

    Added 'standard/load_pressel_legend/' path

--- configurator/urls.py
+++ configurator/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('standard/', views.standard_button, name='stand-butt'),
     path('get_contact_type', views.get_contact_type, name='contact-type'),
     path('standard/load_contact_types/', views.load_contact_types, name='load_contact_types'),
-    path('standard/load_colors/', views.load_colors, name='load_colors')
+    path('standard/load_colors/', views.load_colors, name='load_colors'),
+    path('standard/load_pressel_legend/', views.load_legends, name='load_pressel_legend')
 ]
