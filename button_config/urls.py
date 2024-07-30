cd879019addc5fdc8ad69aa4e7b5d78b92321baa
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2024-07-30 21:45:32 +1000

    Deleted unused paths in button_config/urls.py. Added / before the 'static' directory in button_config/settings.py

--- button_config/urls.py
+++ button_config/urls.py
@@ -4,6 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('configurator.urls')),
-    path('', include('code_testing.urls')),
-    path('', include('ajax_try.urls'))
 ]
