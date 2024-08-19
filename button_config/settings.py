cca0d5fcce1e7e2632a6e5f8899455f5b1f010f6
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2024-08-19 22:05:35 +1000

    Added config.py for environment variables.

--- button_config/settings.py
+++ button_config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+from .config import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4tsodn_ujwd7*r)=^r=kkywao(188o7$==9#c)djl)$m5!7b@j'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'configurator.apps.ConfiguratorConfig',
-    # 'code_testing.apps.CodeTestingConfig',
-    # 'ajax_try.apps.AjaxTryConfig'
+    'configurator.apps.ConfiguratorConfig'
 ]
 
 MIDDLEWARE = [
