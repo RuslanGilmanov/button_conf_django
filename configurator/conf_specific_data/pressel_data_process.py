43d9e18002a8eec1b92f4d9fca27b7f67d54b75b
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2024-09-26 22:48:01 +1000

    Amended pressel_data_process.py, added if __name__ == "__main__" and annotations

--- configurator/conf_specific_data/pressel_data_process.py
+++ configurator/conf_specific_data/pressel_data_process.py
@@ -5,7 +5,15 @@ data = pd.read_excel('configurator/conf_specific_data/pressel_list.xlsx')
 pressel_dict = data.to_dict(orient='index')
 
 
-def get_pressel_types():
+def main() -> None:
+    get_pressel_types()
+    get_pressel_finish()
+    get_polycarbonate_colour()
+    get_pressel_legend()
+    search_in_pressel_dict()
+        
+
+def get_pressel_types() -> list:
     unique_values = data['Type'].unique()
     PRESSEL_TYPES = [("None", "Select pressel type")] 
     for item in unique_values:
@@ -13,7 +21,7 @@ def get_pressel_types():
     return PRESSEL_TYPES
 
 
-def get_pressel_finish(pressel_type):
+def get_pressel_finish(pressel_type) -> list:
      # Using a set for efficient duplicate removal
     pressel_finish_set = {
         sub_dict['Pressel / Plate finish'] 
@@ -25,7 +33,7 @@ def get_pressel_finish(pressel_type):
     return [(finish, finish.title()) for finish  in list(pressel_finish_set)]
 
 
-def get_polycarbonate_colour(pressel_type, pressel_finish):
+def get_polycarbonate_colour(pressel_type, pressel_finish) -> list:
     pressel_polycarb_color_set = {
         sub_dict['Polycarbonate Colour'] 
         for sub_dict in pressel_dict.values() 
@@ -36,7 +44,7 @@ def get_polycarbonate_colour(pressel_type, pressel_finish):
     return [(color, color.title()) for color in list(pressel_polycarb_color_set)]
 
 
-def get_pressel_legend(pressel_type, pressel_finish, polycarb_color):
+def get_pressel_legend(pressel_type, pressel_finish, polycarb_color) -> list:
     pressel_legends = {
         sub_dict['Pressel Legend'] 
         for sub_dict in pressel_dict.values() 
@@ -48,7 +56,7 @@ def get_pressel_legend(pressel_type, pressel_finish, polycarb_color):
     return [(legend, legend) for legend in list(pressel_legends)]
 
 
-def search_in_pressel_dict(pressel_type, pressel_finish, polycarb_color, pressel_legend):
+def search_in_pressel_dict(pressel_type, pressel_finish, polycarb_color, pressel_legend)  -> str:
 
     for key, sub_dict in pressel_dict.items():
         if (sub_dict.get('Type') == pressel_type and
@@ -58,3 +66,7 @@ def search_in_pressel_dict(pressel_type, pressel_finish, polycarb_color, pressel
             return sub_dict['Dewhurst Part Number']
 
     return "Pressel not found."
+
+
+if __name__ == "__main__":
+    main()
